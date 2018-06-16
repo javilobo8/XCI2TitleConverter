42f4358aa06440fd4c238331f47fc7c3fc859548
David Rouleau <367297+Shabbypenguin@users.noreply.github.com>
2018-06-16 10:46:51 -0400

    Define more games titleid

--- XCI2TitleConverter/Constants.cs
+++ XCI2TitleConverter/Constants.cs
@@ -9,6 +9,16 @@ namespace XCI2TitleConverter
             {"010025400AECE000", "Fortnite"},
             {"010043500A17A000", "Fallout Shelter"},
             {"01008D00062C2000", "VOEZ Demo"},
+            {"0100DB7003828000", "Pinball FX3"},
+            {"010096000B3EA000", "Octopath Traveler Demo"},
+            {"01000C900A136000", "Kitten Squad"},
+            {"01005D100807A000", "Pokemon Quest"},
+            {"0100AE0006474000", "Stern Pinball"},
+            {"0100CD300880E000", "Pinball Arcade"},
+            {"0100E65002BB8000", "Stardew Valley"},
+            {"01000A10041EA000", "Skyrim"},
+            {"0100000000010000", "Super Mario Odyssey"},
+            {"01005ee0036ec000", "Rocket League"},
         };
     }
 }
