fe99189fa6581d6f057e02e809b66ec47995a01d
Stoned1337 <Michilein881@hotmail.de>
2018-06-17 16:06:37 +0200

    Update Constants.cs
    Added more Title Ids

--- XCI2TitleConverter/Constants.cs
+++ XCI2TitleConverter/Constants.cs
@@ -22,6 +22,16 @@ namespace XCI2TitleConverter
             {"01005ee0036ec000", "Rocket League"},
             {"0100152000022000", "Mario Kart 8 Deluxe"},
             {"01007EF00011E000", "The Legend of Zelda: Breath of the Wild"},
+            {"0100A5400AC86000", "ARMS Demo"},
+            {"0100BA3003B70000", "NAMCO MUSEUM PAC-MAN VS"},
+            {"0100C360070F6000", "DRAGON QUEST BUILDERS Demo"},
+            {"0100E67003A86000", "Disgaea 5 Complete Demo"},
+            {"0100F5E008AA0000", "LOST SPHEAR Demo"},
+            {"01004AF00A772000", "Monsters 2 Demo"},
+            {"01004E300899E000", "Wanderjahr TryAgainOrWalkAway Demo"},
+            {"01006E30099B8000", "Pic-a-Pix Deluxe Demo"},
+            {"01006EE00AE38000", "Shining Resonance Refrain Demo"},
+            {"010090B005150000", "Oceanhorn Demo"},
         };
     }
 }
